364ecd8723875141a9f01409753596e9cde8f477
visualstudio <visualstudio@122-PC15>
2019-03-08 20:47:49 -0500

    Lab 11

--- jotandlavi/Program.cs
+++ jotandlavi/Program.cs
@@ -11,32 +11,13 @@ namespace ConsoleApp1
 //Assignment 2
 //March 8,2019
 {
-    public class Program
-    {
-        public static void Main()
-        {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
-        }
-    }
-
-    public delegate void MyDelegate();
-
     public class DelegateExercises
     {
-        void Method1()
+        public object MyDelegate { get; private set; }
+
+        void Method3()
         {
-            System.Console.WriteLine("Method1");
-
-
-        }
-
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-            System.Console.WriteLine(myDelegate.ToString());
+            Console.WriteLine(MyDelegate.ToString());
         }
     }
 }
