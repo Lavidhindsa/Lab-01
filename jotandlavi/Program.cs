731d0955df398b96addddad9af4d66217cf8a900
visualstudio <visualstudio@122-PC15>
2019-03-08 19:26:16 -0500

    Lab 03

--- jotandlavi/Program.cs
+++ jotandlavi/Program.cs
@@ -7,24 +7,14 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 //Student name=Harmanjot Singh  Student Id: C0727172
 //Student Name = Labhwinder Singh Student ID: C0728432
+//CSD3354 Section 2
 //Assignment 2
 //March 8,2019
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            DelegateExercises delegateExercises = new DelegateExercises();
-
-            delegateExercises.Method2();
-        }
-    }
-
-
     public class DelegateExercises
     {
         public delegate void MyDelegate();
-        void Method1()
+        void Method1(int i)
         {
             Console.WriteLine("Method1");
             Console.ReadLine();
