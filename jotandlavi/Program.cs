6accb7b5e733790d41e11472fd0436ee120116d1
visualstudio <visualstudio@122-PC15>
2019-03-08 19:14:05 -0500

    Lab 02

--- jotandlavi/Program.cs
+++ jotandlavi/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace jotandlavi
+namespace ConsoleApp1
+//Student name=Harmanjot Singh  Student Id: C0727172
+//Student Name = Labhwinder Singh Student ID: C0728432
+//Assignment 2
+//March 8,2019
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+
+            delegateExercises.Method2();
+        }
+    }
+
+
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
